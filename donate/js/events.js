d4762ada02b3785f702200eba72e9fa23ab03fe8
Dean Zaka Hidayat <deanzaka.h@gmail.com>
2016-10-26 17:28:48 +0700

    Add input restriction to number in donasi form

--- donate/js/events.js
+++ donate/js/events.js
@@ -1,9 +1,18 @@
-var beras = document.getElementById("value");
-var conv = document.getElementById("converse");
+// var beras = document.getElementById("value");
+// var conv = document.getElementById("converse");
 
-function convert() {
-	var val = parseFloat(beras.value) || 0;
-	conv.innerHTML = (val * 10000);
-}
+// function convert() {
+// 	var val = parseFloat(beras.value) || 0;
+// 	conv.innerHTML = (val * 10000);
+// }
 
-value.addEventListener("input", convert);
+// value.addEventListener("input", convert);
+
+$("#value").keyup(function() {
+       var val = $("#value").val() || 0;
+       $("#converse").html(parseFloat(val) * 10000);
+});
+
+$("#value").keyup(function() {
+    $("#value").val(this.value.match(/[0-9]*/));
+});
